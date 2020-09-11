e5a8b7115d8df2641f064888e5a60800e0be6e0c
Gabriel <dreadpool38@gmail.com>
2020-09-11 12:52:29 -0300

    vai

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      {process.env.NODE_ENV === "dev" && <h1>em desenvolvimento</h1>}
       <StrangerThings />
     </div>
   );
