aa21f876719bd6151b15768e7fa68312e7daa82a
Gabriel <dreadpool38@gmail.com>
2020-09-11 14:11:37 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       {process.env.NODE_ENV === "development" && <h1>em desenvolvimento</h1>}
+  <h1>{process.env.NODE_ENV}</h1>
       <StrangerThings />
     </div>
   );
