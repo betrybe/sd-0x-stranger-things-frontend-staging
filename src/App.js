d805f13d6d28c5b7f16759d3404530dcd9d1dfb6
Gabriel <dreadpool38@gmail.com>
2020-09-11 15:16:39 -0300

    vai vaivai

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      {process.env.REACT_APP_ENV === "development" && <h1>em desenvolvimento</h1>}
+      {process.env.REACT_APP_ENV === "development" && <h1>Em desenvolvimento</h1>}
   <h1>{process.env.REACT_APP_ENV}</h1>
       <StrangerThings />
     </div>
